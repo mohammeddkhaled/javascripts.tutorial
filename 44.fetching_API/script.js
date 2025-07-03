11e9353ee4c0297c2e713547e793186ead5d9323
mohammedkhaled <mohdkhaled1031@gmail.com>
2025-07-03 15:23:54 +0530

    catching error

--- 44.fetching_API/script.js
+++ 44.fetching_API/script.js
@@ -3,6 +3,8 @@
 //    return res.json()
 // }).then((data) => {
 //    console.log(data);
+// }).catch((err)=>{
+//     console.log(err);
 // })
 
 
@@ -11,21 +13,26 @@
 // fetch('https://dummyjson.com/products')
 //  .then((res)=> res.json())
 //  .then((products) => console.log(products))
+//  .catch((err)=>console.log(err))
 
 
 
 // post method
 
-// fetch('https://dummyjson.com/products/add', {
-//   method: 'POST',
-//   headers: { 'Content-Type': 'application/json' },
-//   body: JSON.stringify({
-//     title: 'BMW Pencil',
-//     /* other product data */
-//   })
-// })
-// .then(res => res.json())
-// .then(console.log);
+fetch('https://dummyjson.com/products/add', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    title: 'BMW Pencil',
+    /* other product data */
+  })
+})
+.then(res => res.json())
+.then(console.log)
+.catch((err)=>{
+    console.log(err);
+})
+
 
 
 // fetching data using promises
