5b2412216370dc249b9fdbf81497eac8080c2180
mohammedkhaled <mohdkhaled1031@gmail.com>
2025-07-17 12:55:10 +0530

    getter and setter

--- 49.class/script.js
+++ 49.class/script.js
@@ -71,54 +71,102 @@
 
 
 /////////////////////////////////////////////////////////////////////////////
-
-
 // static properties and method on class
 
+// class createUser {
 
+//     constructor(firstName, lastName, age) {
+//         this.firstName = firstName
+//         this.lastName = lastName
+//         this.age = age
+
+//     }
+
+//     static hi = 'hello'
+
+//     // static {     // static block  // it will automatically run 
+
+//     //     console.log('hello');
+//     //     var a = 12
+
+//     //     this.hi = 'hello'
+
+//     //     this.getFullName = function () {
+//     //         return this.firstName + ' ' + this.lastName
+//     //     }
+
+//     // }
+
+//     getBirthYear() {
+//         return new Date().getFullYear() - this.age
+//     }
+
+//     static getFullName(){
+//         return this.firstName + ' ' + this.lastName
+//     }
+
+
+// }
+
+// const user1 = new createUser('mohammed', 'khaled', 23)
+
+// const user2 = new createUser('mohammed', 'mateen', 24)
+
+// console.log(user1);
+// console.log(user2);
+
+/////////////////////////////////////////////////////////////////////////
+
+// getter and setter
+
+// const user = {
+//     firstName: 'mohammed',
+//     lastName: 'khaled',
+//     get fullName() {
+//         return `${this.firstName} ${this.lastName}`
+
+//     },
+//     // set fullName (value){
+//     //    const  splitted = value.split(' ')
+//     //    console.log(splitted);
+//     //    this.firstName = splitted[0]
+//     //    this.lastName = splitted[1]
+//     // }
+//     //        or
+//      set fullName(value) {
+//     const [firstName , lastName] = value.split(' ')
+//     this.firstName = firstName
+//     this.lastName =lastName
+// }
+// }
+// // console.log(user.firstName+ " " + user.lastName);
+// //or
+// // console.log(`${user.firstName} ${user.lastName}`);
+// user.fullName = 'mohammed mateen'
+
+////////////////////////////////
+
+// getter and setter with classes
 class createUser {
-
     constructor(firstName, lastName, age) {
         this.firstName = firstName
         this.lastName = lastName
         this.age = age
 
     }
-
-    static hi = 'hello'
-
-    // static {     // static block  // it will automatically run 
-
-    //     console.log('hello');
-    //     var a = 12
-
-    //     this.hi = 'hello'
-
-    //     this.getFullName = function () {
-    //         return this.firstName + ' ' + this.lastName
-    //     }
-
-    // }
-
     getBirthYear() {
         return new Date().getFullYear() - this.age
     }
-
-    static getFullName(){
-        return this.firstName + ' ' + this.lastName
+    get FullName() {
+        return `${this.firstName}  ${this.lastName}`
     }
-
-
+    set fullName(value) {
+        const [firstName, lastName] = value.split(' ')
+        this.firstName = firstName
+        this.lastName = lastName
+    }
 }
-
-const user1 = new createUser('mohammed', 'khaled', 23)
-
-const user2 = new createUser('mohammed', 'mateen', 24)
-
-console.log(user1);
-console.log(user2);
-
-
+const user1 = new createUser('mohammed', 'khaled', 24)
 
 
 
